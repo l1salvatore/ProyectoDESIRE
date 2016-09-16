d82cfe66c8bd93dad0cdd64a36a1279017e15e5c
l1salvatore <lucas_salva_15@hotmail.com>
2016-09-16 16:24:22 -0300

    facil

--- USB.py
+++ USB.py
@@ -1,4 +1,15 @@
+import evdev,os,signal,sys
+
+
 class usb:
+	def __init__(self,dev):
+		self.device = evdev.InputDevice(dev)
+		print(self.device)
 	def escuchar(self):
-		return "vacio"
-		
+		os.system("stty -echo")
+		for event in self.device.read_loop():
+			if event.type == evdev.ecodes.EV_KEY:
+				if event.code == evdev.ecodes.KEY_A and event.value == 1:
+					print("una A presionada\n")
+				elif event.code == evdev.ecodes.KEY_B and event.value == 1:
+					print("una B presionada\n")
--- vacio.py
+++
@@ -1,9 +0,0 @@
-import evdev
-
-device = evdev.InputDevice('/sys/devices/pci000xyz/000.000.XYZ/usbX/X-Y')
-print(device)
-
-
-for event in device.read_loop():
-     if event.type == evdev.ecodes.EV_KEY:
-         print(evdev.categorize(event))
