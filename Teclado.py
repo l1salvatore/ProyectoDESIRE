9987943d6c00ecb9768c3ad30915693226e62c29
Lucas <lucas@lucas-desktop.(none)>
2016-09-20 23:11:37 -0300

    USB Listo

--- Teclado.py
+++ Teclado.py
@@ -1,11 +1,16 @@
-import USB,evdev
+import USB,evdev,threading,sys
+
+global codigo
+codigo = ""
 
 class Teclado:
 	def __init__(self,port):
 		self.puerto = USB.usb(port)
-	def scanf(self):
-		eventcode = self.puerto.obtener_evento()
-		if eventcode == evdev.ecodes.KEY_A:
-			return 'A'
-		elif eventcode == evdev.ecodes.KEY_B:
-			return 'B' 
+	def iniciar_teclado(self):
+		p = threading.Thread(target=self.puerto.obtener_evento,args=(codigo,))
+	        p.start()
+		while 1:
+		    if codigo == evdev.ecodes.KEY_A:
+			print('A')
+		    elif codigo == evdev.ecodes.KEY_B:
+			print('B')
--- USB.py
+++ USB.py
@@ -5,7 +5,11 @@ class usb:
 	def __init__(self,dev):
 		self.device = evdev.InputDevice(dev)
 		print(self.device)
-	def obtener_evento(self):
-		for event in self.device.read():
+	def obtener_evento(self,codigo):
+		for event in self.device.read_loop():
 			if event.type == evdev.ecodes.EV_KEY and event.value == 1:
-					return event.code
+				codigo = event.code
+			    	#if event.code == evdev.ecodes.KEY_A:
+				#    print('A')
+				#elif event.code == evdev.ecodes.KEY_B:
+				#    print('B') 
